d1038c28f122a48f4e3a12f651d1e0ede2f5cf05
Serene Lim <enerestar@gmail.com>
2019-08-26 12:23:15 +0800

    1.0.3

--- release.sh
+++ release.sh
@@ -5,14 +5,15 @@ GH_PATH=$GITHUB_API_TOKEN
 GH_REPO=homebrew-enerestar
 GH_TARGET=master
 ASSETS_PATH=.
-VERSION=1.0.2
+VERSION=1.0.3
 tar --exclude='./.git' --exclude='./README.md' --exclude="enerestar-${VERSION}.tar.gz" --exclude='./.circleci' -zcvf "enerestar-${VERSION}.tar.gz" .
 
 git add -u
 git commit -m "$VERSION release"
 git push
 
-res=`curl --user "$GH_USER:$GH_PATH" -X POST https://api.github.com/repos/${GH_USER}/${GH_REPO}/releases -d "
+res=`curl --user "$GH_USER:$GH_PATH" -X POST https://api.github.com/repos/${GH_USER}/${GH_REPO}/releases \
+-d "
 {
   \"tag_name\": \"v$VERSION\",
   \"target_commitish\": \"$GH_TARGET\",
